2d5edbfbba27a2b6a7f27ed7a1bab5ac29f41ed8
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2021-07-18 23:38:25 +0530

    sideImage + details + newsletter component

--- glass-newsletter/src/App.js
+++ glass-newsletter/src/App.js
@@ -1,10 +1,26 @@
-import './App.css';
+import "./App.css";
+import styled from "styled-components";
+import { NewsLetter } from "./components/newsLetter";
+
+const AppContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: rgb(81, 103, 134);
+  background: linear-gradient(
+    180deg,
+    rgba(81, 103, 134, 1) 21%,
+    rgba(28, 29, 45, 1) 100%
+  );
+`;
 
 function App() {
   return (
-    <div className="App">
-    
-    </div>
+    <AppContainer>
+      <NewsLetter />
+    </AppContainer>
   );
 }
 
